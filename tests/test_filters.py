85e2aa26debfbd896a7c9c80ede39119ff9f1941
DanielGTdiabetes <danieltellols@gmail.com>
2025-08-27 07:46:47 +0200

    Delete tests directory

--- tests/test_filters.py
+++
@@ -1,7 +0,0 @@
-from bascula.config.settings import FilterConfig
-from bascula.domain.filters import ProfessionalWeightFilter
-
-def test_filter_basic():
-    f = ProfessionalWeightFilter(FilterConfig())
-    out = f.step(0.0)
-    assert isinstance(out.display, float)
