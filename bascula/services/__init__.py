7519b606ab60d2451d5e93564d3dae70832bad1d
DanielGTdiabetes <danieltellols@gmail.com>
2025-08-31 10:44:23 +0200

    Update __init__.py

--- bascula/services/__init__.py
+++ bascula/services/__init__.py
@@ -1,1 +1,11 @@
-# bascula.services package
+# -*- coding: utf-8 -*-
+"""
+Paquete de servicios para la aplicación báscula.
+
+Este paquete agrupa clases y utilidades que abstraen funcionalidades
+auxiliares, como acceso a la cámara. De momento sólo incluye
+CameraService, que gestiona la cámara Raspberry Pi a través de
+Picamera2.
+"""
+
+__all__ = ["camera"]
