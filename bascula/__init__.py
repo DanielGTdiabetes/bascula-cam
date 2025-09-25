366e89078e78a84f3955349c71259aec21bf6ebe
DanielGTdiabetes <danieltellols@gmail.com>
2025-09-25 18:41:32 +0200

    install: normalize bascula-web deployment

--- bascula/__init__.py
+++ bascula/__init__.py
@@ -1,2 +1,12 @@
-# Paquete 'bascula'
-from . import utils
+"""Top level package for the Bascula application stack."""
+
+from importlib import metadata as _metadata
+
+try:  # pragma: no cover - metadata only available when installed
+    __version__ = _metadata.version("bascula")
+except _metadata.PackageNotFoundError:  # pragma: no cover - editable installs
+    __version__ = "0.0.0"
+
+from . import utils  # noqa: F401 - re-export for backwards compatibility
+
+__all__ = ["__version__", "utils"]
